166ecf26b5064988865bac448509be8dbd530806
JuicyColt <51383990+JuicyColt@users.noreply.github.com>
2020-01-04 10:12:10 +0300

    Update Fibonacci.lisp

--- lab1/Fibonacci.lisp
+++ lab1/Fibonacci.lisp
@@ -1,5 +1,4 @@
-(defun fib (n)
-  (check-type n (integer 0 *))
-  (loop for f1 = 0 then f2
-        and f2 = 1 then (+ f1 f2)
-        repeat n finally (return f1)))
+(defun fibonacci (n &optional (a 0) (b 1))
+  (if (zerop n)
+      nil
+      (cons a (fibonacci (1- n) b (+ a b)))))
